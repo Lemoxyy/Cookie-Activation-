975c21c49e8805a95cb8a2e02a8729b5b1d22bfd
Lemoxyy 69 <92450755+Lemoxyy@users.noreply.github.com>
2023-02-20 21:37:33 +0545

    Update Cookies.py

--- Cookies.py
+++ Cookies.py
@@ -1,2 +1,9 @@
 cookies = input("Please enter your name for cookies: ")
-print(f"Cookies is Activated{cookies}")
+print(f"Cookies is Activated by {cookies}")
+print("Enter the Value for X")
+x = int(input("Enter x: ")) 
+print("Enter the Value for Y")
+y = int(input("Enter y: "))
+z = x + y 
+
+print(f"The sum of {x} and {y} is {z}")
